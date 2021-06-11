06ae9eb51b72a7cbc7f52689ea1a4341adc60196
joshua <joshuajohnmorris@gmail.com>
2021-06-11 13:35:24 +1000

    Two sum

---
+++ LeetCode/0001_TwoSum.java
@@ -0,0 +1,34 @@
+// Brute force O(n^2)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
+
+// With map O(n)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            int current = target - nums[i];
+            if (map.containsKey(current) && map.get(current) != i) {
+                return new int[] {i, map.get(current)};
+            }
+        }
+        return null;
+    }
+}
